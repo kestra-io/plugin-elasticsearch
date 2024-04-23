6119aa3432d9ce0e852909cdd8d0aef592362a76
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-04-23 22:08:36 +0200

    refactor: migrate to Java ServiceLoader for plugin registration

--- src/main/java/io/kestra/plugin/elasticsearch/ElasticsearchConnection.java
+++ src/main/java/io/kestra/plugin/elasticsearch/ElasticsearchConnection.java
@@ -4,7 +4,6 @@ import io.kestra.core.exceptions.IllegalVariableEvaluationException;
 import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.utils.VersionProvider;
-import io.micronaut.core.annotation.Introspected;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -34,7 +33,6 @@ import jakarta.validation.constraints.NotNull;
 @SuperBuilder
 @NoArgsConstructor
 @Getter
-@Introspected
 public class ElasticsearchConnection {
     @Schema(
         title = "List of HTTP ElasticSearch servers.",
@@ -85,7 +83,6 @@ public class ElasticsearchConnection {
     @SuperBuilder
     @NoArgsConstructor
     @Getter
-    @Introspected
     public static class BasicAuth {
         @Schema(
             title = "Basic auth username."
