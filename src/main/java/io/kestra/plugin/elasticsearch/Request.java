1e072e21da99f018a1512b605394adca1255efcf
Florian Hussonnois <florian.hussonnois@gmail.com>
2024-04-23 22:08:36 +0200

    refactor: migrate to Java ServiceLoader for plugin registration

--- src/main/java/io/kestra/plugin/elasticsearch/Request.java
+++ src/main/java/io/kestra/plugin/elasticsearch/Request.java
@@ -7,7 +7,7 @@ import io.kestra.core.models.annotations.PluginProperty;
 import io.kestra.core.models.tasks.RunnableTask;
 import io.kestra.core.runners.RunContext;
 import io.kestra.core.serializers.JacksonMapper;
-import io.micronaut.http.HttpMethod;
+import io.kestra.plugin.elasticsearch.model.HttpMethod;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
---
+++ src/main/java/io/kestra/plugin/elasticsearch/model/HttpMethod.java
@@ -0,0 +1,17 @@
+package io.kestra.plugin.elasticsearch.model;
+
+/**
+ * HTTP methods.
+ */
+public enum HttpMethod {
+    CONNECT,
+    CUSTOM,
+    DELETE,
+    GET,
+    HEAD,
+    OPTIONS,
+    PATCH,
+    POST,
+    PUT,
+    TRACE;
+}
