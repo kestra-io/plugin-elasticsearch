e8ba7d7e64fc621507e644a394178dc0681792a6
ben8t <pimpaudben@gmail.com>
2025-12-30 15:37:16 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/elasticsearch/package-info.java
+++ src/main/java/io/kestra/plugin/elasticsearch/package-info.java
@@ -1,7 +1,11 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for using ElasticSearch.\n" +
         "ElasticSearch is a distributed, RESTful search and analytics engine capable of addressing a growing number of use cases.",
-    categories = PluginSubGroup.PluginCategory.DATABASE
+    categories = PluginSubGroup.PluginCategory.DATABASE,
+    categories = {
+        PluginSubGroup.PluginCategory.DATA,
+        PluginSubGroup.PluginCategory.INFRASTRUCTURE
+    }
 )
 package io.kestra.plugin.elasticsearch;
 
